75fbad5419d096c3c094e7a8f7177bb6b9bdd71b
Harry Zachariou <harryzachariou1@gmail.com>
2023-03-30 18:06:41 +0100

    remove ;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
         />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   );
 }
