d00d1616ec38d13c4f6bcf5795de1ac5d1fcd9ad
Harry Zachariou <harryzachariou1@gmail.com>
2023-03-30 18:06:41 +0100

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌎</text></svg>"
+        />
+
         <title>Your 10,000th Day</title>
         <meta property="og:title" content="Your 10,000th Day" key="title" />
         <meta
